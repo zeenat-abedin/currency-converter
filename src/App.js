34947e0f0238eb5110fdd2f34ea06c6a0077ee3f
zeenat-abedin <zeenu.23@gmail.com>
2024-01-08 17:29:41 +0530

    Update dependency array

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
       setResult(data.rates[targetCurrency]);
     }
     convert();
-  }, []);
+  }, [amount, baseCurrency, targetCurrency]);
 
   return (
     <div>
