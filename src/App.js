3eb71a550727cea94fa062a1905ac3b84b848ec0
zeenat-abedin <zeenu.23@gmail.com>
2024-01-08 15:48:05 +0530

    Added state variables and event handlers for user input in the currency converter app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
 export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [baseCurrency, setBaseCurrency] = useState("EUR");
+  const [targetCurrency, setTargetCurrency] = useState("USD");
+
   return (
     <div>
-      <input type="text" />
+      <input type="text" value={(e) => setAmount(e.target.value)} />
       <select>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
